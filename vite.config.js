988ec18f35ff2e28410064790afa56e1dd6d1f8b
gacallea <gacallea@users.noreply.github.com>
2022-05-23 17:39:39 +0200

    exlcuding compress for now, bugs are blocking

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import elmPlugin from 'vite-plugin-elm'
 import { ViteWebfontDownload } from 'vite-plugin-webfont-dl'
-import compress from 'vite-plugin-compress'
+// import compress from 'vite-plugin-compress'
 
 export default defineConfig({
   plugins: [
     elmPlugin(),
-    ViteWebfontDownload(),
-    // vite-plugin-compress has bugs:
+    ViteWebfontDownload()
+    // vite-plugin-compress has serious blocking bugs:
     // https://github.com/alloc/vite-plugin-compress/issues/10
     // https://github.com/alloc/vite-plugin-compress/issues/15
-    compress({
-      exclude: ['**.svg']
-    })
+    // compress({
+    //   exclude: ['**.svg']
+    // })
   ]
 })
