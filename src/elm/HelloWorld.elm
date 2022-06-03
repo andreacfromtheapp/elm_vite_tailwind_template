60eccb3ca475ab0dbbd2ca9edd7b77c396eb9c12
gacallea <gacallea@users.noreply.github.com>
2022-06-03 10:27:10 +0200

    added tailwind docs link, some refactor

--- src/elm/HelloWorld.elm
+++ src/elm/HelloWorld.elm
@@ -10,16 +10,16 @@ import Msg exposing (Msg(..))
 
 helloWorld : Int -> Html Msg
 helloWorld model =
-    div [ class "py-2" ]
-        [ h1 [ class "text-lg font-medium text-center" ] [ text "Hello, Vite + Tailwind + Elm!" ]
-        , p []
+    div [ class "py-2 text-center" ]
+        [ h1 [ class "text-lg font-medium" ] [ text "Hello, Vite + Tailwind + Elm!" ]
+        , p [ class "p-2 flex flex-col" ]
             [ a [ href "https://vitejs.dev/guide/features.html" ] [ Icon.viewIcon Icon.book, text " Vite Documentation" ]
-            , text " | "
             , a [ href "https://guide.elm-lang.org/" ] [ Icon.viewIcon Icon.book, text " Elm Documentation" ]
+            , a [ href "https://tailwindcss.com/docs/installation" ] [ Icon.viewIcon Icon.book, text " Tailwind Documentation" ]
             ]
         , div [ class "text-center" ]
-            [ button [ class "p-2", onClick Decrement ] [ text "-" ]
+            [ button [ class "m-2 px-2 bg-slate-300 hover:bg-slate-400 border border-black", onClick Decrement ] [ text "-" ]
             , text <| "Count is: " ++ String.fromInt model
-            , button [ class "p-2", onClick Increment ] [ text "+" ]
+            , button [ class "m-2 px-2 bg-slate-300 hover:bg-slate-400 border border-black", onClick Increment ] [ text "+" ]
             ]
         ]
