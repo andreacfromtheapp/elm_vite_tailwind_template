06f6ca8c20a51449180059ba681549ccb73bf95d
gacallea <gacallea@users.noreply.github.com>
2022-06-10 17:37:58 +0200

    minor refactoring

--- src/elm/HelloWorld.elm
+++ src/elm/HelloWorld.elm
@@ -2,24 +2,46 @@ module HelloWorld exposing (helloWorld)
 
 import FontAwesome.Icon as Icon
 import FontAwesome.Solid as Icon
-import Html exposing (Html, a, button, div, h1, p, text)
-import Html.Attributes exposing (class, href)
+import Html exposing (Html, a, button, div, h1, img, table, td, text, tr)
+import Html.Attributes exposing (class, href, src)
 import Html.Events exposing (onClick)
 import Msg exposing (Msg(..))
+import VitePluginHelper
+
+
+elmLogo : String
+elmLogo =
+    VitePluginHelper.asset "../img/Elm_logo.svg"
 
 
 helloWorld : Int -> Html Msg
 helloWorld model =
-    div [ class "py-2 text-center" ]
-        [ h1 [ class "text-lg font-medium" ] [ text "Hello, Vite + Tailwind + Elm!" ]
-        , p [ class "p-2 flex flex-col" ]
-            [ a [ href "https://vitejs.dev/guide/features.html" ] [ Icon.viewIcon Icon.book, text " Vite Documentation" ]
-            , a [ href "https://guide.elm-lang.org/" ] [ Icon.viewIcon Icon.book, text " Elm Documentation" ]
-            , a [ href "https://tailwindcss.com/docs/installation" ] [ Icon.viewIcon Icon.book, text " Tailwind Documentation" ]
-            ]
-        , div [ class "text-center" ]
+    div [ class "text-xl" ]
+        [ h1 [ class "text-center text-4xl mb-4" ] [ text "Hello, Vite + Tailwind + Elm!" ]
+        , img [ src elmLogo, class "w-80 mx-auto" ] []
+        , div [ class "text-center mt-4" ]
             [ button [ class "m-2 px-2 bg-slate-300 hover:bg-slate-400 border border-black", onClick Decrement ] [ text "-" ]
             , text <| "Count is: " ++ String.fromInt model
             , button [ class "m-2 px-2 bg-slate-300 hover:bg-slate-400 border border-black", onClick Increment ] [ text "+" ]
+            ]
+        , table [ class "table-auto mt-4 text-left mx-auto" ]
+            [ tr []
+                [ td []
+                    [ Icon.viewIcon Icon.book ]
+                , td []
+                    [ a [ href "https://vitejs.dev/guide/features.html" ] [ text "Vite Documentation" ] ]
+                ]
+            , tr []
+                [ td []
+                    [ Icon.viewIcon Icon.book ]
+                , td []
+                    [ a [ href "https://guide.elm-lang.org/" ] [ text "Elm Documentation" ] ]
+                ]
+            , tr []
+                [ td []
+                    [ Icon.viewIcon Icon.book ]
+                , td []
+                    [ a [ href "https://tailwindcss.com/docs/installation" ] [ text "Tailwind Documentation" ] ]
+                ]
             ]
         ]
--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -4,20 +4,25 @@ import Accessibility.Aria as Aria
 import Browser
 import FontAwesome.Styles as Icon
 import HelloWorld exposing (helloWorld)
-import Html exposing (Html, div, img)
-import Html.Attributes exposing (class, src)
+import Html exposing (Html, div)
+import Html.Attributes exposing (class)
 import Msg exposing (Msg(..))
-import VitePluginHelper
-
-
-elmLogo : String
-elmLogo =
-    VitePluginHelper.asset "../img/Elm_logo.svg"
 
 
 main : Program () Int Msg
 main =
     Browser.sandbox { init = 0, update = update, view = view }
+
+
+view : Int -> Html Msg
+view model =
+    div
+        [ Aria.label "main content"
+        , class "flex flex-col justify-center items-center h-screen font-sans"
+        ]
+        [ Icon.css
+        , helloWorld model
+        ]
 
 
 update : Msg -> number -> number
@@ -28,15 +33,3 @@ update msg model =
 
         Decrement ->
             model - 1
-
-
-view : Int -> Html Msg
-view model =
-    div
-        [ Aria.label "main content"
-        , class "flex flex-col justify-center items-center h-screen font-sans"
-        ]
-        [ Icon.css
-        , img [ src elmLogo, class "w-80" ] []
-        , helloWorld model
-        ]
